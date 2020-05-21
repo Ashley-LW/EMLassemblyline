cd163e4c02c73a95857a687f583b1305e1d8b7a9
clnsmth <colin.smith@wisc.edu>
2020-05-20 17:26:11 -0700

    Continue fixing 15

--- R/build_spatialRaster.R
+++ R/build_spatialRaster.R
@@ -58,21 +58,43 @@ build_spatialRaster <- function(
   # Get spatial reference and convert to EML standard--------------------------
   
   # First read the proj4 string
-  # Read: https://www.nceas.ucsb.edu/sites/default/files/2020-04/OverviewCoordinateReferenceSystems.pdf
   proj4str <- raster::crs(rasterObject, asText = T)
   print(paste('Spatial reference in proj4 format is:', proj4str))
-  
-  # browser()
   
   # Assign EML-compliant name - a manually determined translation of proj4str
   # Allowed values for EML seem to be enumerated here: 
   #  https://eml.ecoinformatics.org/schema/eml-spatialReference_xsd.html#SpatialReferenceType
   # Searching projection names at https://spatialreference.org is helpful
   # for the translation
+
+  # Try and match proj4str from object to eml_horizCoordSysDef --------------
+  # TODO: Do values outside the emlProjection invalidate the EML?
+  # TODO: How were the enumerated emlProject values created?
+  
+  # Get EML coordinate list
+  eml_horizCoordSysDef <- get_coord_list()
+  
+  # browser()
+  
+  # TODO: Add this to template_spatial_raster_attributes() so the ESRIWKT
+  # value can be manually added if spatialreference.org is offline
+  #
+  # Get mapping between EPSG projection codes and proj4
+  epsg <- rgdal::make_EPSG()
+  # Get EPSG for proj4
+  use_i <- !is.na(epsg$prj4) & (epsg$prj4 == proj4str)
+  # Convert EPSG to ESRIWKT
+  r <- httr::GET(
+    url = "https://spatialreference.org/ref/epsg/4326/esriwkt/")
+  output <- httr::content(r, as = 'text', encoding = 'UTF-8')
+  # Extract GEOGCS from ESRIWKT
+
+  proj <- rgdal::projInfo("proj")
+  datum <- rgdal::projInfo("datum")
+  ellps <- rgdal::projInfo("ellps")
+
   if (proj4str=="+proj=utm +zone=13 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"){
     emlProjection <- "NAD_1983_UTM_Zone_13N"
-  } else if (proj4str=="+proj=utm +zone=13 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"){
-    emlProjection <- "NAD_1983_CSRS98_UTM_Zone_13N"
   }
   print(paste("Translated to", emlProjection, 'in EML spatialReference schema'))
   
@@ -189,8 +211,8 @@ build_spatialRaster <- function(
     numberOfBands = bandnr(rasterObject),
     rows = nrow(rasterObject),
     columns = ncol(rasterObject),
-    horizontalAccuracy = EML::eml$horizontalAccuracy(accuracyReport="Unknown"),
-    verticalAccuracy = EML::eml$verticalAccuracy(accuracyReport="Unknown"),
+    horizontalAccuracy = EML::eml$horizontalAccuracy(accuracyReport="Unknown"), # Add field in template
+    verticalAccuracy = EML::eml$verticalAccuracy(accuracyReport="Unknown"), # Add field in template
     cellSizeXDirection = xres(rasterObject),
     cellSizeYDirection = yres(rasterObject),
     rasterOrigin = "Upper Left",
@@ -202,3 +224,80 @@ build_spatialRaster <- function(
   return(newSR)
 }
 
+
+
+# Jeanette Clark's helpers ----------------------------------------------------
+
+#' Get list of EML Coordinate Reference Systems
+#'
+#' Get a data.frame of EML coordinate reference systems that can
+#' be searched and filtered more easily than the raw XML file.
+#'
+#' @export
+#'
+get_coord_list <- function(){
+  geo_list <- EML::read_eml("https://raw.githubusercontent.com/NCEAS/eml/4417cbf6588fdca4e06bd67190a9d7a18a8e944f/eml-spatialReferenceDictionary.xml")
+  coord_df <- data.frame(horizCoordSysDef = rep(NA, length(geo_list$horizCoordSysDef)),
+                         geogCoordSys = rep(NA, length(geo_list$horizCoordSysDef)),
+                         projection = rep(NA, length(geo_list$horizCoordSysDef)),
+                         datum = rep(NA, length(geo_list$horizCoordSysDef)),
+                         proj_unit = rep(NA, length(geo_list$horizCoordSysDef)))
+  for (i in 1:length(geo_list$horizCoordSysDef)){
+    coord_df$horizCoordSysDef[i] <- geo_list$horizCoordSysDef[[i]]$name
+    if (!is.null(geo_list$horizCoordSysDef[[i]]$projCoordSys)){
+      coord_df$geogCoordSys[i]  <- geo_list$horizCoordSysDef[[i]]$projCoordSys$geogCoordSys$name
+      coord_df$datum[i]  <- geo_list$horizCoordSysDef[[i]]$projCoordSys$geogCoordSys$datum$name
+      coord_df$projection[i] <- geo_list$horizCoordSysDef[[i]]$projCoordSys$projection$name
+      coord_df$proj_unit[i] <- geo_list$horizCoordSysDef[[i]]$projCoordSys$projection$unit$name
+    } else {
+      coord_df$geogCoordSys[i]  <- geo_list$horizCoordSysDef[[i]]$geogCoordSys$name
+      coord_df$datum[i]  <- geo_list$horizCoordSysDef[[i]]$geogCoordSys$datum$name
+      coord_df$projection[i] <- NA
+      coord_df$proj_unit[i] <- NA
+    }
+  }
+  return(coord_df)
+}
+
+#' Get raster info from a file on disk
+#'
+#' This function populates a spatialRaster element with the
+#' required elements by reading a local raster file in. The
+#' `coord_name` argument can be found by examining the data.frame
+#' that `get_coord_list()` returns against the proj4string of the
+#' raster file.
+#'
+#' @param path (char) Path to a raster file
+#' @param coord_name (char) horizCoordSysDef name
+#' @param attributes (data.frame) attribute list as a data.frame for raster
+#'
+#'
+#' @export
+eml_get_raster_metadata <- function(path, coord_name = NULL, attributes){
+  raster_obj <- raster::raster(path)
+  
+  message(paste("Reading raster object with proj4string of ", raster::crs(raster_obj)@projargs))
+  if (is.null(coord_name)){
+    coord_name <- raster::crs(raster_obj)@projargs
+  }
+  if (identical(raster::origin(raster_obj), c(0,0))){
+    raster_orig <- "Upper Left"
+  } else if(!identical(raster::origin(raster_obj), c(0,0))){
+    message("Raster origin not at 0,0")
+    raster_orig <- "unknown"
+  }
+  raster_info <- list(entityName = basename(path),
+                      attributeList = set_attributes(attributes),
+                      spatialReference = list(horizCoordSysName = coord_name),
+                      horizontalAccuracy = list(accuracyReport = "unknown"),
+                      verticalAccuracy = list(accuracyReport = "unknown"),
+                      cellSizeXDirection = raster::res(raster_obj)[1],
+                      cellSizeYDirection = raster::res(raster_obj)[2],
+                      numberOfBands = raster::nbands(raster_obj),
+                      rasterOrigin = raster_orig,
+                      rows = dim(raster_obj)[1],
+                      columns = dim(raster_obj)[2],
+                      verticals = dim(raster_obj)[3],
+                      cellGeometry = "pixel")
+  return(raster_info)
+}
